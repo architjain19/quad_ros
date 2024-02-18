ed2621852b3979f6bf18e29787dd0b7e63b650be
Archit Jain <arrchit.jain@gmail.com>
2024-02-18 15:55:23 +0530

    fix: added alternate foot control (trot method)

--- quad_control/quad_control/custom_controller.py
+++ quad_control/quad_control/custom_controller.py
@@ -18,11 +18,13 @@ class CustomController(Node):
         self.joint_limits = math.radians(120)
         self.joint_names = ['thigh_joint_fr', 'leg_joint_fr', 'thigh_joint_fl', 'leg_joint_fl', 'thigh_joint_rr', 'leg_joint_rr', 'thigh_joint_rl', 'leg_joint_rl']
         self.command_functions = [
-            (self.exec_custom_stance_phase_commands, (0.0, 0.1, 0.375, 0.25, 5, 0.05)),
-            (self.exec_custom_stance_phase_commands, (0.1, 0.2, 0.25, 0.375, 5, 0.05)),
-            (self.exec_custom_stance_phase_commands, (0.2, 0.0, 0.375, 0.375, 5, 0.05))
+            (self.generate_custom_stance_phase_trajectory, (0.0, 0.1, 0.375, 0.25, 2)),
+            (self.generate_custom_stance_phase_trajectory, (0.1, 0.2, 0.25, 0.375, 2)),
+            (self.generate_custom_stance_phase_trajectory, (0.2, 0.0, 0.375, 0.375, 2))
         ]
+        self.leg_num = 4
         self.current_command_index = 0
+        self.static_angle1, self.static_angle2 = self.calculate_inverse_kinematics(0.0, 0.375)
         
     def calculate_inverse_kinematics(self, x, y):
         '''
@@ -64,7 +66,10 @@ class CustomController(Node):
                 trajectory_msg = JointTrajectory()
                 trajectory_msg.joint_names = self.joint_names
                 point = JointTrajectoryPoint()
-                point.positions = [angle1, angle2]*4
+                if self.current_command_index == 0 or self.current_command_index == 2:
+                    point.positions = [angle1, angle2, self.static_angle1, self.static_angle2, self.static_angle1, self.static_angle2, angle1, angle2]
+                elif self.current_command_index == 1 or self.current_command_index == 3:
+                    point.positions = [self.static_angle1, self.static_angle2, angle1, angle2, angle1, angle2, self.static_angle1, self.static_angle2]
                 point.time_from_start.sec = 1
                 trajectory_msg.points.append(point)
                 self.publisher_group.publish(trajectory_msg)
@@ -84,34 +89,30 @@ class CustomController(Node):
         y_values = np.linspace(y1, y2, num_points)
         return list(zip(x_values, y_values))
 
-    def exec_custom_stance_phase_commands(self, x1, x2, y1, y2, num_points, delay_trajectory):
-        '''
-        Calculate vertical stance trajectory points 
-        and calculate joint angles using inverse kinematics over these x, y points 
-        and send the joint command to respective topics 
-        '''
-        trajectory = self.generate_custom_stance_phase_trajectory(x1, x2, y1, y2, num_points)
-        self.trace_trajectory_path(delay_trajectory, trajectory)
+    def get_single_step_traj(self):
+        result_traj = []
+        for command in self.command_functions:
+            result = command[0](*command[1])
+            result_traj.extend(result)
+        return result_traj
 
     def loop_control(self):
         if not self.trace_trajectory_cb_status:
-            self.get_logger().info(f'Executing next command...')
-            function, parameters = self.command_functions[self.current_command_index]
-            function(*parameters)
-            self.current_command_index = (self.current_command_index + 1) % len(self.command_functions)
+            self.get_logger().info(f'Executing next step...')
+            trajectory = self.get_single_step_traj()
+            self.trace_trajectory_path(0.5, trajectory)
+            self.current_command_index = (self.current_command_index + 1) % self.leg_num
         else:
             # self.get_logger().info(f'Waiting for timer to complete...')
             pass
 
     def custom_gait(self, delay_loop):
-        # self.exec_custom_stance_phase_commands(0.0, 0.1, 0.35, 0.25, 5)
-        # print(self.trace_trajectory_cb_status)
         loop_control_timer = self.create_timer(delay_loop, self.loop_control)
 
 def main(args=None):
     rclpy.init(args=args)
     custom_controller = CustomController()
-    custom_controller.custom_gait(1)     # timer=1
+    custom_controller.custom_gait(1)
     rclpy.spin(custom_controller)
     rclpy.shutdown()
 
